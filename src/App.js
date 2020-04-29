67f3d32a6b3de0107885d9fbcd0117f15d05ee3a
Saloni <suparekh01@gmail.com>
2020-04-29 20:26:46 +0530

    Css added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <form id="to-do-form">
+            <input type="text" placeholder="Enter Text" />
+            <button type="submit">Add</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
